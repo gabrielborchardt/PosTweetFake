ef210bf1920c46cfad62d9783ea980b3cdf42f04
Gabriel Borchardt <gabriel.borchardt@hotmail.com>
2019-05-11 12:04:01 -0300

    Update Tweet.js

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -17,6 +17,7 @@ export default class Tweet {
           <div class="content">
             ${this.text}
           </div>
+          <button class="button is-black is-pulled-right">Excluir</button>
         </div>
       </div>
     `
@@ -28,6 +29,19 @@ export default class Tweet {
     const cardContainer = this.card(this.name, this.text)
     tweet.innerHTML = cardContainer
     this.tweet = tweet
+
+    this.deleteButton = this.tweet.getElementsByTagName('button')[0]
+    this.binds()
+    
     return tweet
   }
+
+  deleteTweet () {
+    this.tweet.remove()
+  }
+
+  binds () {
+    this.deleteButton.addEventListener('click', this.deleteTweet.bind(this))
+  }
+
 }
